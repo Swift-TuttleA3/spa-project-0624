c01362fc8d0362e2f52af1bf45c788883aeda518
Swift-TuttleA3 <r.panek888@gmail.com>
2024-06-20 19:56:14 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header'
+import Middle from './components/Middle'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Middle />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header>
+          <h1>My Header</h1>
+          <NavBar />
+    </header>
+  );
+};
+
+export default Header;
+
